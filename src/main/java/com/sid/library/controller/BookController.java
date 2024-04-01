77fd099bf852b67e1709355fd5411fbec5ee07f7
siddhartha behera <siddhartha.behera@yahoo.co.in>
2024-04-01 08:50:19 +0100

    New property added.

--- src/main/java/com/sid/library/controller/BookController.java
+++ src/main/java/com/sid/library/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.sid.library.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +15,18 @@ import java.util.List;
 @RequestMapping("/api/books")
 public class BookController {
 
+    @Value("${app.greeting}")
+    private String msg;
+
     @GetMapping
     public ResponseEntity getAllBooks() {
         return new ResponseEntity(Arrays.asList(new Book(1, "Life of cat", "Vivaan Behera"),new Book(2, "Life of dog", "Siddhartha Behera")), HttpStatus.OK);
     }
+    @GetMapping(path = "/msg")
+    public ResponseEntity getAuthorMessage() {
+        return new ResponseEntity(msg, HttpStatus.OK);
+    }
+
+
 
 }
