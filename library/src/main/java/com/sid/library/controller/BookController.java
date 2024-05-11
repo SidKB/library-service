7ebe0e9b4902bceaed88ded88e69a3efe9c072bc
siddhartha behera <siddhartha.behera@yahoo.co.in>
2024-05-11 20:46:26 +0100

    Added compute code

--- library/src/main/java/com/sid/library/controller/BookController.java
+++ library/src/main/java/com/sid/library/controller/BookController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -21,6 +23,12 @@ public class BookController {
     @Value("${app.greeting}")
     private String msg;
 
+    @Value("${app.minScale:1000}")
+    private Integer minScale;
+
+    @Value("${app.minScale:5000}")
+    private Integer maxScale;
+
     @GetMapping
     public ResponseEntity getAllBooks() {
         return new ResponseEntity(Arrays.asList(new Book(1, "Life of cat", "Vivaan Behera"),new Book(2, "Life of dog", "Siddhartha Behera")), HttpStatus.OK);
@@ -29,18 +37,14 @@ public class BookController {
     public ResponseEntity getAuthorMessage() {
 
         Random random = new Random();
-        // Define minimum and maximum delay in milliseconds
-        int minDelay = 1000; // 1 second
-        int maxDelay = 5000; // 5 seconds
 
-        int randomDelay = random.nextInt(maxDelay - minDelay + 1) + minDelay;
-        log.info("Simulating work with random delay of " + randomDelay + " milliseconds");
-        try {
-            Thread.sleep(randomDelay);
-        } catch (InterruptedException e) {
-            log.error("InterruptedException",e);
-        }
-        log.info("Work finished!");
+        int randomScale = random.nextInt(maxScale - minScale + 1) + minScale;
+        BigDecimal bigNumber = new BigDecimal(Math.PI).setScale(randomScale);
+        log.info("Simulating CPU workload with large number calculations (scale: " + randomScale + ")");
+
+        // Perform complex calculations on bigNumber (example: square root)
+        BigDecimal squareRoot = bigNumber.sqrt(MathContext.DECIMAL32);
+        log.info("CPU workload finished!");
 
         return new ResponseEntity(msg, HttpStatus.OK);
     }
