179ca431cea5027ebf5e1c1804675b48dc899dbf
siddhartha behera <siddhartha.behera@yahoo.co.in>
2024-05-11 20:19:38 +0100

    Added logic to add execution time

--- library/src/main/java/com/sid/library/controller/BookController.java
+++ library/src/main/java/com/sid/library/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.sid.library.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
+@Log4j2
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
@@ -24,6 +27,21 @@ public class BookController {
     }
     @GetMapping(path = "/msg")
     public ResponseEntity getAuthorMessage() {
+
+        Random random = new Random();
+        // Define minimum and maximum delay in milliseconds
+        int minDelay = 1000; // 1 second
+        int maxDelay = 5000; // 5 seconds
+
+        int randomDelay = random.nextInt(maxDelay - minDelay + 1) + minDelay;
+        log.info("Simulating work with random delay of " + randomDelay + " milliseconds");
+        try {
+            Thread.sleep(randomDelay);
+        } catch (InterruptedException e) {
+            log.error("InterruptedException",e);
+        }
+        log.info("Work finished!");
+
         return new ResponseEntity(msg, HttpStatus.OK);
     }
 
